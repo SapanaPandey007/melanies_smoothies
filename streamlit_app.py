a81635233e02b3faece0f8a025c4c47bc6fdbfd8
SapanaPandey007 <sapana.pandey@diacto.com>
2024-06-09 15:35:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -31,6 +30,9 @@ if  ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!',icon="✅")
 
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 
 
